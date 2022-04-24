93495755157824af492dcc3730ce6c1aa5f1828a
escamoteur <github@burkharts.net>
2022-04-24 17:40:07 +0200

    added controls

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
-import 'dart:typed_data';
-import 'dart:ui' as ui;
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_mandel/mandelbrot.dart';
+
+import 'mandel_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,102 +15,129 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Mandel Demo'),
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
         ),
-        body: AspectRatio(
-          aspectRatio: 4 / 3,
-          child: LayoutBuilder(
-            builder: (BuildContext context, BoxConstraints constraints) {
-              return MandleView(
-                  width: constraints.maxWidth.toInt(),
-                  height: constraints.maxHeight.toInt());
-            },
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Mandel Demo'),
           ),
-        ),
-      ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter,
-      //   tooltip: 'Increment',
-      //   child: const Icon(Icons.add),
-      // ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+          body: const MandelExplorer(),
+        )
+        //   onPressed: _incrementCounter,
+        //   tooltip: 'Increment',
+        //   child: const Icon(Icons.add),
+        // ), // This trailing comma makes auto-formatting nicer for build methods.
+        );
   }
 }
 
-class MandleView extends StatefulWidget {
-  final int width;
-  final int height;
-  const MandleView({
-    required this.width,
-    required this.height,
+class MandelExplorer extends StatefulWidget {
+  const MandelExplorer({
     Key? key,
   }) : super(key: key);
 
   @override
-  State<MandleView> createState() => _MandleViewState();
+  State<MandelExplorer> createState() => _MandelExplorerState();
 }
 
-class _MandleViewState extends State<MandleView> {
-  late final imageBuffer = Uint32List(widget.width * widget.height);
-
-  @override
-  void initState() {
-    imageBuffer.fillRange(0, imageBuffer.length, 0xff00ff00);
-
-    final mandel = Mandelbrot();
-
-    mandel.renderData(
-        data: imageBuffer,
-        xMin: -2.4,
-        xMax: 0.5,
-        yMin: -1.1,
-        yMax: 1.1,
-        bitmapWidth: widget.width,
-        bitMapHeight: widget.height);
-
-    super.initState();
-  }
+class _MandelExplorerState extends State<MandelExplorer> {
+  Offset upperLeft = const Offset(-2.05, -1.1);
+  double renderWidth = 3.0;
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<ui.FrameInfo>(
-        future:
-            ui.ImmutableBuffer.fromUint8List(imageBuffer.buffer.asUint8List())
-                .then((value) => ui.ImageDescriptor.raw(value,
-                        width: widget.width,
-                        height: widget.height,
-                        pixelFormat: ui.PixelFormat.bgra8888)
-                    .instantiateCodec()
-                    .then((codec) => codec.getNextFrame())),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return Container(
-              color: Colors.amber,
-              child: RawImage(
-                image: snapshot.data!.image,
-                fit: BoxFit.cover,
-              ),
+    return Stack(
+      children: [
+        LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+            return MandleView(
+              width: constraints.maxWidth.toInt(),
+              height: constraints.maxHeight.toInt(),
+              upperLeftCoord: upperLeft,
+              renderWidth: renderWidth,
             );
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        });
+          },
+        ),
+        Positioned(
+            right: 60,
+            bottom: 100,
+            child: FloatingActionButton(
+              mini: true,
+              onPressed: (() {
+                setState(() {
+                  upperLeft = upperLeft.translate(0, -(renderWidth / 30));
+                });
+              }),
+              child: const Icon(Icons.arrow_drop_up),
+            )),
+        Positioned(
+            right: 20,
+            bottom: 60,
+            child: FloatingActionButton(
+              mini: true,
+              onPressed: (() {
+                setState(() {
+                  upperLeft = upperLeft.translate((renderWidth / 30), 0);
+                });
+              }),
+              child: const Icon(Icons.arrow_right),
+            )),
+        Positioned(
+            right: 100,
+            bottom: 60,
+            child: FloatingActionButton(
+              mini: true,
+              onPressed: (() {
+                setState(() {
+                  upperLeft = upperLeft.translate(-(renderWidth / 30), 0);
+                });
+              }),
+              child: const Icon(Icons.arrow_left),
+            )),
+        // floatingActionButton: FloatingActionButton(
+        Positioned(
+            right: 60,
+            bottom: 20,
+            child: FloatingActionButton(
+              mini: true,
+              onPressed: (() {
+                setState(() {
+                  upperLeft = upperLeft.translate(0, (renderWidth / 30));
+                });
+              }),
+              child: const Icon(Icons.arrow_drop_down),
+            )),
+        Positioned(
+            right: 85,
+            bottom: 150,
+            child: FloatingActionButton(
+              mini: true,
+              onPressed: (() {
+                setState(() {
+                  renderWidth = renderWidth - (renderWidth / 30);
+                  upperLeft = upperLeft.translate(
+                      (renderWidth / 60), (renderWidth / 60));
+                });
+              }),
+              child: const Icon(Icons.zoom_in),
+            )),
+        // floatingActionButton: FloatingActionButton(
+        Positioned(
+            right: 35,
+            bottom: 150,
+            child: FloatingActionButton(
+              mini: true,
+              onPressed: (() {
+                setState(() {
+                  renderWidth = renderWidth + 0.1;
+                  upperLeft = upperLeft.translate(-0.05, -0.05);
+                });
+              }),
+              child: const Icon(Icons.zoom_out),
+            ))
+      ],
+    );
   }
 }
---
+++ lib/mandel_view.dart
@@ -0,0 +1,82 @@
+import 'dart:typed_data';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+import 'mandelbrot.dart';
+
+class MandleView extends StatefulWidget {
+  final int width;
+  final int height;
+  final Offset upperLeftCoord;
+  final double renderWidth;
+  const MandleView({
+    required this.width,
+    required this.height,
+    required this.upperLeftCoord,
+    required this.renderWidth,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<MandleView> createState() => _MandleViewState();
+}
+
+class _MandleViewState extends State<MandleView> {
+  Uint32List? imageBuffer;
+  final mandel = Mandelbrot();
+  FrameInfo? frameToDisplay;
+
+  @override
+  void initState() {
+    imageBuffer = Uint32List(widget.width * widget.height);
+    renderMandel();
+
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(covariant MandleView oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.width != oldWidget.width || widget.height != oldWidget.height) {
+      /// Window size changed so we have to adapt our image buffer size
+      imageBuffer = Uint32List(widget.width * widget.height);
+    }
+    renderMandel();
+  }
+
+  void renderMandel() {
+    final double aspect = widget.width / widget.height;
+
+    mandel.renderData(
+        data: imageBuffer!,
+        xMin: widget.upperLeftCoord.dx,
+        xMax: widget.upperLeftCoord.dx + widget.renderWidth,
+        yMin: widget.upperLeftCoord.dy,
+        yMax: widget.upperLeftCoord.dy + widget.renderWidth / aspect,
+        bitmapWidth: widget.width,
+        bitMapHeight: widget.height);
+
+    ImmutableBuffer.fromUint8List(imageBuffer!.buffer.asUint8List())
+        .then((value) => ImageDescriptor.raw(value,
+                width: widget.width,
+                height: widget.height,
+                pixelFormat: PixelFormat.bgra8888)
+            .instantiateCodec()
+            .then((codec) => codec.getNextFrame()))
+        .then((frame) => setState(() => frameToDisplay = frame));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (frameToDisplay != null) {
+      return RawImage(
+        image: frameToDisplay!.image,
+      );
+    } else {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+  }
+}
